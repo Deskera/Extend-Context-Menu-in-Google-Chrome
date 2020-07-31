c2c5af287c9d1e53298ff8f6596b147611b01a74
Brajesh Sachan <brajesh.sachan@gmail.com>
2020-07-31 16:51:38 +0800

    Conditional context menu

--- background.js
+++ background.js
@@ -9,9 +9,28 @@ function getword(info,tab) {
   });
 }
 console.log("CONTEXT_MENU_ID: " + CONTEXT_MENU_ID);
-chrome.contextMenus.create({
-  title: "Open \"%s\" in Jira", 
-  contexts:["selection"],
-  id: CONTEXT_MENU_ID
+
+var cmid;
+chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
+  if (msg.request === 'updateContextMenu') {
+      if (msg.selection == '') {
+          if (cmid != null) {
+              chrome.contextMenus.remove(cmid);
+              cmid = null;
+          }
+      } else {
+          var options = {
+              title: "Open \"%s\" in Jira",
+              contexts: ['selection']
+          };
+          if (cmid != null) {
+              chrome.contextMenus.update(CONTEXT_MENU_ID, options);
+          } else {
+              options.id = CONTEXT_MENU_ID;
+              chrome.contextMenus.create(options);
+              cmid = CONTEXT_MENU_ID;
+          }
+      }
+  }
 });
 chrome.contextMenus.onClicked.addListener(getword);
