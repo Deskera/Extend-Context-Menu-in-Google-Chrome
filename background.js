366938cddf444207b504c78bf051c14dac82320c
Brajesh Sachan <brajesh.sachan@gmail.com>
2020-07-31 18:18:47 +0800

    Show context menu on specific selection

--- background.js
+++ background.js
@@ -1,11 +1,16 @@
+const target = "Jira";
+const targetUrl = "https://jira.deskera.com/browse/";
+const selectionMatchingPattern = /\w+-\d+/g;
+
 const CONTEXT_MENU_ID = "CUSTOM_CONTEXT_MENU_" + Date.now();
-function getword(info,tab) {
+
+function contextMenuHandler(info,tab) {
   if (info.menuItemId !== CONTEXT_MENU_ID) {
     return;
   }
-  console.log("Word " + info.selectionText + " was clicked.");
+  console.log(info.selectionText + " was clicked.");
   chrome.tabs.create({  
-    url: "https://jira.deskera.com/browse/" + info.selectionText
+    url: targetUrl + info.selectionText
   });
 }
 console.log("CONTEXT_MENU_ID: " + CONTEXT_MENU_ID);
@@ -13,24 +18,24 @@ console.log("CONTEXT_MENU_ID: " + CONTEXT_MENU_ID);
 var cmid;
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
   if (msg.request === 'updateContextMenu') {
-      if (msg.selection == '') {
-          if (cmid != null) {
-              chrome.contextMenus.remove(cmid);
-              cmid = null;
-          }
+    if (msg.selection.match(selectionMatchingPattern)) {
+      var options = {
+        title: "Open \"%s\" in " + target,
+        contexts: ['selection']
+      };
+      if (cmid != null) {
+        chrome.contextMenus.update(CONTEXT_MENU_ID, options);
       } else {
-          var options = {
-              title: "Open \"%s\" in Jira",
-              contexts: ['selection']
-          };
-          if (cmid != null) {
-              chrome.contextMenus.update(CONTEXT_MENU_ID, options);
-          } else {
-              options.id = CONTEXT_MENU_ID;
-              chrome.contextMenus.create(options);
-              cmid = CONTEXT_MENU_ID;
-          }
+        options.id = CONTEXT_MENU_ID;
+        chrome.contextMenus.create(options);
+        cmid = CONTEXT_MENU_ID;
       }
+    } else {
+      if (cmid != null) {
+        chrome.contextMenus.remove(cmid);
+        cmid = null;
+      }
+    }
   }
 });
-chrome.contextMenus.onClicked.addListener(getword);
+chrome.contextMenus.onClicked.addListener(contextMenuHandler);
